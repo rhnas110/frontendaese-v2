9b40c1be27bd4e9f92b9c25614230e20e4c53919
rhnas110 <raihanas110@gmail.com>
2024-05-17 10:47:16 +0700

    refactor: Image Elements user cant copy image

--- src/components/Elements/Image/index.tsx
+++ src/components/Elements/Image/index.tsx
@@ -15,7 +15,7 @@ export const Image = ({
       src={src}
       alt={alt || "Alt Image"}
       className={cn(
-        "object-cover object-center w-full h-full bg-[#BED2F5]",
+        "object-cover object-center w-full h-full bg-[#BED2F5] select-none",
         className
       )}
       loading={lazy ? "lazy" : "eager"}
