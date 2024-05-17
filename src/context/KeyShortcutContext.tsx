35363c7c2ad9be1915fba216b739d35caf711045
rhnas110 <raihanas110@gmail.com>
2024-05-17 16:21:49 +0700

    feat: install KeyShortcutContext

---
+++ src/context/KeyShortcutContext.tsx
@@ -0,0 +1,37 @@
+// KeyShortcutContext.tsx
+import { createContext, FC, ReactNode, useContext, useState } from "react";
+import useShortcutKey from "../hooks/useShortcutKey";
+
+interface KeyShortcutContextType {
+  showSidebar: boolean;
+}
+
+const KeyShortcutContext = createContext<KeyShortcutContextType | undefined>(
+  undefined
+);
+
+export const KeyShortcutProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  useShortcutKey({
+    modifierKeys: ["Control"],
+    keys: ["b"],
+    action: () => setShowSidebar(!showSidebar),
+  });
+
+  return (
+    <KeyShortcutContext.Provider value={{ showSidebar }}>
+      {children}
+    </KeyShortcutContext.Provider>
+  );
+};
+
+export const useKeyShortcut = () => {
+  const context = useContext(KeyShortcutContext);
+  if (!context) {
+    throw new Error("useKeyShortcut must be used within a KeyShortcutProvider");
+  }
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import { TabProvider } from "./context/TabContext";
 import { SidebarProvider } from "./context/SidebarContext.tsx";
+import { KeyShortcutProvider } from "./context/KeyShortcutContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Router>
         <TabProvider>
           <SidebarProvider>
-            <App />
+            <KeyShortcutProvider>
+              <App />
+            </KeyShortcutProvider>
           </SidebarProvider>
         </TabProvider>
       </Router>
