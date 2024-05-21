555608ecba6ff3bf4aaaf02549564690b11d32ef
rhnas110 <raihanas110@gmail.com>
2024-05-21 19:24:25 +0700

    feat: add image in ModalOnceUp

--- src/components/Layouts/Modal/ModalOnceUp.tsx
+++ src/components/Layouts/Modal/ModalOnceUp.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Modal from "../../_ui/Modal";
+import { Image } from "../../Elements/Image";
 
 export const ModalOnceUp = () => {
   const [open, setOpen] = useState(false);
@@ -24,11 +25,19 @@ export const ModalOnceUp = () => {
   return (
     <Modal open={open} onOpenChange={handleOpen}>
       <Modal.Content title="Hello World!">
-        <div className="flex flex-col gap-2 text-center text-gray-300">
+        <div className="relative flex flex-col gap-2 text-center text-gray-300">
           <p>
             Try opening this site on a device with a width of 1024px or more!
           </p>
-          <p>There will be interesting things that you will like</p>
+          <p>There will be interesting things that you will like.</p>
+          <div className="absolute -top-6 -right-12">
+            <Image
+              alt="hello"
+              src="https://media.tenor.com/PEp7__gqEYoAAAAi/mythikore-anime-girl.gif"
+              className="w-40 bg-transparent opacity-50"
+              lazy
+            />
+          </div>
           <button
             onClick={handleOpen}
             className="p-2 mt-4 text-white rounded bg-aese-900"
