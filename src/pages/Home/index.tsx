09f7eae2f4fec95bb088760c8a30d0543428ad3f
rhnas110 <raihanas110@gmail.com>
2024-05-21 13:56:24 +0700

    feat: use lenis smooth scroll

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import { Home as HomeLayouts } from "../../components/Layouts/Home";
 import PageWrapper from "../../components/PageWrapper";
+import useLenis from "../../hooks/useLenis";
 
 const Home = () => {
+  useLenis();
   return (
     <PageWrapper>
       <HomeLayouts />
