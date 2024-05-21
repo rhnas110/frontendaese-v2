f2e1e946dc1022cfae4b57f9678d26961f0fc919
rhnas110 <raihanas110@gmail.com>
2024-05-21 14:38:40 +0700

    style: font semibold for Modal.Title className style

--- src/components/_ui/Modal/index.tsx
+++ src/components/_ui/Modal/index.tsx
@@ -31,7 +31,7 @@ function ModalContent({
       <Dialog.Overlay className="fixed inset-0 z-20 DialogOverlay bg-background/50 backdrop-blur-sm" />
       <Dialog.Content className="lg:p-8 p-4 shadow fixed z-20 w-[95vw] max-w-md -translate-x-1/2 -translate-y-1/2 border rounded border-background DialogContent bg-backgroundSecondary left-1/2 top-1/2">
         <div className="relative flex items-center justify-center mb-4 text-gray-400">
-          <Dialog.Title className="text-xl">{title}</Dialog.Title>
+          <Dialog.Title className="text-xl font-semibold">{title}</Dialog.Title>
           <Dialog.Close
             className="absolute top-0 right-0 hover:text-gray-500"
             asChild
