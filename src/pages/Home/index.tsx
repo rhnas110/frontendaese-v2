443eee7c4cb7dcec382a3db4b235f7a1037ebd4e
rhnas110 <raihanas110@gmail.com>
2024-05-23 17:15:34 +0700

    fix: remove Lenis its not sync

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,7 @@
 import { Home as HomeLayouts } from "../../components/Layouts/Home";
 import PageWrapper from "../../components/PageWrapper";
-import useLenis from "../../hooks/useLenis";
 
 const Home = () => {
-  useLenis();
   return (
     <PageWrapper>
       <HomeLayouts />
