41b4351bcfbec14cd5b0b58f10a1512df9068ba8
rhnas110 <raihanas110@gmail.com>
2026-02-14 17:45:25 +0700

    chore: add full screen utility

--- src/utils/screen.ts
+++ src/utils/screen.ts
@@ -1,14 +1,14 @@
 function getWindowWidth() {
   return Math.max(
     document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
+    window.innerWidth || 0,
   );
 }
 
 function getWindowHeight() {
   return Math.max(
     document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
+    window.innerHeight || 0,
   );
 }
 
@@ -31,4 +31,40 @@ export {
   MobileLg,
   Tablet,
   Desktop,
+};
+
+interface FsDocumentElement extends HTMLElement {
+  msRequestFullscreen?: () => void;
+  mozRequestFullScreen?: () => void;
+  webkitRequestFullscreen?: () => void;
+}
+
+interface FsDocument extends Document {
+  webkitIsFullScreen?: boolean;
+  mozFullScreen?: boolean;
+  msFullscreenElement?: Element;
+}
+
+export const enterFullScreen = (): void => {
+  if (!isFullScreen()) {
+    const element = document.documentElement as FsDocumentElement;
+    if (element.requestFullscreen) element.requestFullscreen();
+    else if (element.msRequestFullscreen) element.msRequestFullscreen();
+    else if (element.mozRequestFullScreen) element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+  }
+};
+
+export const exitFullScreen = (): void => {
+  if (isFullScreen()) document.exitFullscreen();
+};
+
+export const isFullScreen = (): boolean => {
+  const fsDoc = document as FsDocument;
+  return !!(
+    fsDoc.fullscreenElement ||
+    fsDoc.webkitIsFullScreen ||
+    fsDoc.mozFullScreen ||
+    fsDoc.msFullscreenElement
+  );
 };
